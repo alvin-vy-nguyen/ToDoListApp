483cd959033d9a9c6802b2a1beeeb8f802d33895
Alvin Nguyen <alvin.nguyen@veeva.com>
2024-02-18 12:11:01 -0800

    Added MVC with Item Model, attached properties to string name in array

---
+++ ToDoList-iOS17/Data Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  ToDoList-iOS17
+//
+//  Created by Alvin Nguyen on 2/16/24.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
--- ToDoList-iOS17/Views/TodoListView.swift
+++ ToDoList-iOS17/Views/TodoListView.swift
@@ -8,18 +8,34 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-
-    var itemArray = ["To DO Monday", "Starting Tuesday", "Finishing Wednesday"]
+    
+    // Initialized an array of Item objects
+    var itemArray = [Item]()
     
     // Establish user defaults
     let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+        //"To DO Monday", "Starting Tuesday", "Finishing Wednesday"
+        // Create a new item object of type Item from Item class
+        let newItem = Item()
+        newItem.title = "To DO Monday"
+        itemArray.append(newItem)
+        
+        let newItem1 = Item()
+        newItem1.title = "Starting Tuesday"
+        itemArray.append(newItem1)
+        
+        let newItem2 = Item()
+        newItem2.title = "Finishing Wednesday"
+        itemArray.append(newItem2)
+        
+        // When app loads up, we look into our user defaults and pull whatever is there from previous session
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = items
         }
+        
     }
     
     //MARK: - Tableview Datasource Methods
@@ -29,27 +45,37 @@ class TodoListViewController: UITableViewController {
         
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        let item = itemArray[indexPath.row]
+        // Our row will now return an Item Object that contains multiple fields, so we need to tap into the title
+        cell.textLabel?.text = item.title
+        
+        // Swift Ternary Operator, if item.done is true set to .checkmark else .none
+        cell.accessoryType = item.done == true ? .checkmark : .none // Does below more elegant
+        
         return cell
         
     }
     
     //MARK: - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        // Gets the current item object thats selected
+        // Reverses the state of the .done
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done // Better way of writing than below
+//        if itemArray[indexPath.row].done == false {
+//            itemArray[indexPath.row].done = true
+//        } else {
+//            itemArray[indexPath.row].done = false
+//        }
+        
+        tableView.reloadData()
+        
         // Animation for deselecting selected row
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
     
     //MARK: - Add New Items
-    
-    
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
@@ -58,7 +84,10 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // What happens when user clicks on the "add item" button on our UIAlert
-            self.itemArray.append(textField.text!)
+            // We create a new item object and append the item (that includes whatever the user types in)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             
             // Save user defaults
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
