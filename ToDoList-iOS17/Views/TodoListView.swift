40079ec1d203e665cb62dab973c5f0de99c9b63b
Alvin Nguyen <alvin.nguyen@veeva.com>
2024-02-19 16:03:46 -0800

    Added CoreData Model and converted ItemArray to store in SQLite DB with context save

--- ToDoList-iOS17/Views/TodoListView.swift
+++ ToDoList-iOS17/Views/TodoListView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     
@@ -14,24 +15,17 @@ class TodoListViewController: UITableViewController {
     
     // .default is a Singleton, .userDomainMask is their homeDirectory
     // Creates our own plist at the data location point
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    // let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    // Tap into "UIApplication" object, get the "shared" singleton object which is the current app as an object
+    // We then tap into it's delegate which has data type UIApplicationDelegate and we downcast it as AppDelegate since they both inherit from the same superclass UIApplicationDelegate
+    // Then we get access to our app delegate as an object, and can get it property persistentContainer
+    // This is all to get access to the persistentContainer as shown in our AppDelegate saveContext() method
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Create a new item object of type Item from Item class
-//        let newItem = Item()
-//        newItem.title = "To DO Monday"
-//        itemArray.append(newItem)
-//        
-//        let newItem1 = Item()
-//        newItem1.title = "Starting Tuesday"
-//        itemArray.append(newItem1)
-//        
-//        let newItem2 = Item()
-//        newItem2.title = "Finishing Wednesday"
-//        itemArray.append(newItem2)
-        
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         loadItems()
     }
     
@@ -54,11 +48,22 @@ class TodoListViewController: UITableViewController {
     }
     
     //MARK: - TableView Delegate Methods
+    // Gets the current item object thats selected
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // Gets the current item object thats selected
-        // Reverses the state of the .done
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        // Example of updating title in our database
+        // itemArray[indexPath.row].setValue("Completed", forKey: "title")
         
+        // Reverses the state of the .done
+        // Sets the value of our .done key in our database to either 0 or 1
+         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        // Below is to remove the data from our database (from our currently selected row)
+        // Have to call context.delete first, to make sure index isn't out of range when we call itemArray.remove
+        /*context.delete(itemArray[indexPath.row])*/ // Note, this does nothing until we SAVE the context
+        // Below just modifies the itemArray for display on our app
+        // itemArray.remove(at: indexPath.row)
+        
+        // Calls context.save() to save our modifications to the context
         saveItems()
         
         // Animation for deselecting selected row
@@ -67,6 +72,7 @@ class TodoListViewController: UITableViewController {
     
     
     //MARK: - Add New Items
+    // What happens when user clicks on the "add item" button on our UIAlert
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
@@ -74,10 +80,10 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New To-do Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            // What happens when user clicks on the "add item" button on our UIAlert
             // We create a new item object and append the item (that includes whatever the user types in)
-            let newItem = Item()
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
+            newItem.done = false
             self.itemArray.append(newItem)
             
             self.saveItems()
@@ -98,32 +104,27 @@ class TodoListViewController: UITableViewController {
     //MARK: - Model Manipulation Methods
     
     func saveItems() {
-        // Encodes the data to write to a plist
-        let encoder = PropertyListEncoder()
-        
-        // .encode and .write can throw, so we need to enclose them in a do/catch with a try
         do {
-            let data = try encoder.encode(itemArray)
-            // Write data to data file path (creates a .plist upon writing)
-            try data.write(to: dataFilePath!)
+            // Transfers from "scratchpad" to permanent memory
+            try context.save()
         } catch {
-            print("Error encoding item array, \(error)")
+            print("Error saving content \(error)")
         }
         
         // Reloads the rows and the sections of the table taking into account new data that was created
         self.tableView.reloadData()
     }
     
-    // Function that implements decoding
     func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-                itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("error decoding item array, \(error)")
-            }
+        // Need to specify the type of the Fetch Request for our Item Object
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+            // Save results of the fetch into itemArray
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
         }
+        
     }
 }
 
