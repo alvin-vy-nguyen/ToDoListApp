a1b9a0704febebd7643f164fee38a7ddcaa2298e
Alvin Nguyen <alvin.nguyen@veeva.com>
2024-02-19 20:13:34 -0800

    Added searchBar that can query for [cd] text, includes clearing text

--- ToDoList-iOS17/Views/TodoListView.swift
+++ ToDoList-iOS17/Views/TodoListView.swift
@@ -26,6 +26,7 @@ class TodoListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
         loadItems()
     }
     
@@ -115,16 +116,49 @@ class TodoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems() {
-        // Need to specify the type of the Fetch Request for our Item Object
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
+    // Loading function that has an extenral parameter of "with" and a default value
+    // of Item.fetchRequest()
+    // Also specifies the type of the Fetch Request for our Item Object
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
         do {
             // Save results of the fetch into itemArray
             itemArray = try context.fetch(request)
         } catch {
             print("Error fetching data from context \(error)")
         }
-        
+        tableView.reloadData()
     }
 }
 
+//MARK: - Search Bar methods
+extension TodoListViewController: UISearchBarDelegate {
+    // Triggered when user taps on the search button
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        // %@ gets replaced by our "arguments", NSPredicate searches itemArray where the title contains what we have in searchBar when we search
+        // [cd] allows for case (upper/lower case) and diacritic (accents/marks) insensitivity
+        // Query generation with NSPredicate
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        // Sort Descriptor for our query
+        // .sortDescriptors is plural and requires an array
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+    }
+    
+    // Only triggers when text changes (not on first load-up when its empty)
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            // Fetches all items with our default query
+            loadItems()
+            
+            // Runs this method on the main Queue
+            DispatchQueue.main.async {
+                // No longer have cursor & keyboard goes away
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+}
