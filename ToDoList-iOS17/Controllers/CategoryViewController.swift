24ea4c24fe313ab706d8938716b6c756c1bd29e7
Alvin Nguyen <alvin.nguyen@veeva.com>
2024-02-20 20:24:28 -0800

    Initial setup of additional category TableView and additional CoreData Entity

---
+++ ToDoList-iOS17/Controllers/CategoryViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CategoryViewController.swift
+//  ToDoList-iOS17
+//
+//  Created by Alvin Nguyen on 2/20/24.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    //MARK: - TabkeView Datasouce Methods
+    // Display all categories inside container
+    
+    //MARK: - Data Manipulation Methods
+    //Save Data/ Load Data using CRUD
+    
+    //MARK: - Add New Categories
+    // Setup IBAction using new entity
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    //MARK: - TableView Delegate Methods
+    // What should happen when we click on one of the cells
+}
